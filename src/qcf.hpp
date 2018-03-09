ae3e9f7d7ebf5d3647e9955ba6d2e9785676f5b4
Paul Blischak <paul.blischak@gmail.com>
2018-03-09 09:19:33 -0500

    fixing patch number to 0.1.3a

--- src/qcf.hpp
+++ src/qcf.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 /* Version info. */
-const std::string VERSION = "0.1.2a";
+const std::string VERSION = "0.1.3a";
 const std::string DATE    = "March 2018";
 
 void usage(); /* Print usage/help information. */
